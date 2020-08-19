3206cea4d18f3ea69be1826a44a01170c131e6c5
Nikoly <turnik3580@gmail.com>
2020-08-19 14:42:16 +0300

    added code

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketcCost = 100_00_0955;
+        int eachSpentMoney = 20;
+        int mile = 1;
+        int bonus = ticketcCost / eachSpentMoney * mile;
+
+        System.out.println(bonus);
+    }
+}
